b477e81b74325049be42497b8ec30afcedff36a9
clean-command-1851 <ed7901979t@gmail.com>
2019-07-27 17:16:34 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,30 @@
 def addition(num1, num2)
-
+  num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  num1 - num2
 end
 
 def division(num1, num2)
-
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
 
 def modulo(num1, num2)
-
+  num1 % num2
 end
 
 def square_root(num)
-
+ Math.sqrt(num) 
 end
+
+addition(5,4)
+subtraction(5,4)
+division(25,5)
+multiplication(5,4)
+modulo(25,3)
+square_root(25)
